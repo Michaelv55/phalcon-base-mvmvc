e9689b770e4473c9ed408aa391d55ce1e4f065e6
Michael Velasco <maicolvelasco55@gmail.com>
2022-11-08 12:16:48 -0500

    config the app services for tests

--- app/tests/AbstractUnitTest.php
+++ app/tests/AbstractUnitTest.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 use Phalcon\Incubator\Test\PHPUnit\UnitTestCase;
 use PHPUnit\Framework\IncompleteTestError;
 use Phalcon\Di\FactoryDefault;
+use Phalcon\Di;
 
 abstract class AbstractUnitTest extends UnitTestCase {
 
@@ -12,11 +13,25 @@ abstract class AbstractUnitTest extends UnitTestCase {
 
     protected function setUp(): void {
         try {
-            parent::setUp();
+            $this->checkExtension('phalcon');
+
+            // Reset the DI container
+            Di::reset();
+
+            // Instantiate a new DI container
+            $diBase = new Di();
+
             if(!in_array($this->getPhalconConfigPath(), get_included_files())){
                 include $this->getPhalconConfigPath();
                 self::setPhalconDi($di);
             }
+
+            // set the app services for tests
+            foreach (self::getPhalconDi()->getServices() as $name => $service) {
+                $diBase->setService($name, $service);
+            }
+
+            $this->setDI($diBase);
         } catch (\Exception $ex) {
             throw new IncompleteTestError('Error to running app (phalcon) configuration.', 500, $ex);
         }
